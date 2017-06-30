5466136532c7a1fdb7f16b8a935483f8dec22ad1
rdshoep <rdshoep@gmail.com>
2017-06-30 16:32:16 +0800

    remove dist version folder add dist to build add gulp tasks

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: "/dist/",
-    filename: package.version + '/[name].js'
+    filename: '[name].js'
   },
   node: {
     fs: "empty",
